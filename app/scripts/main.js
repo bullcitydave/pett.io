7f3359f576a29f39d0e0aed3fb07a02b4afcb545
Dave Seidman <bullcitydave@gmail.com>
2014-08-01 13:46:32 -0400

    Updated API URL

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,12 @@
 // LOAD FLICKR VARIABLES
 var apiKey = "806745a8a5db2aff0b0cdb591b633726";
 
-var flickrUserID = '125739404@N08';
+var flickrUserID = 'toastie97';
 
 // var flickrApiUrl = "https://api.flickr.com/services/rest?method=flickr.favorites.getPublicList&api_key=" + apiKey + "&user_id=" + flickrUserID + "&safe_search=1&per_page=20";
 
-var flickrApiUrl =  "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6f0d1c3a1af34340afb9f07a98a5764d&user_id=toastie97&tags=moksha&per_page=16&page=1&format=json&nojsoncallback=1&auth_token=72157646088307324-96e2e58d93f54a9f&api_sig=9f50c252c4f28926cc5127cfa3677dbc";
+var flickrApiUrl =  "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + apiKey + "&user_id=" + flickrUserID + "&tags=moksha&per_page=16&page=1&format=json&nojsoncallback=1";
+console.log("API call to ", flickrApiUrl);
 
 // LOAD FLICKR COLLAGE
 
@@ -14,7 +15,7 @@ var flickrApiUrl =  "https://api.flickr.com/services/rest/?method=flickr.photos.
      var random = Math.round(Math.random()*12);
     $('.montageSquare:eq('+random+')').css({opacity: 1.0});
      var random2 = Math.round(Math.random()*12);
-    $('.montageSquare:eq('+random2+')').css({opacity: 0.1});
+    $('.montageSquare:eq('+random2+')').css({opacity: 0.4});
   }
 
 
