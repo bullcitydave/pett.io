a905e20bbc943ffa838b7556a6a6a30aca7e47ed
Dave Seidman <bullcitydave@gmail.com>
2014-08-09 21:24:34 -0400

    Initial app view, with known user, now uses router to go straight to pet page; incidental temporary styling change to pet pic rendering

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,14 +2,17 @@ $(function() {
 
   var AppView = Parse.View.extend({
 
+    pet: "zellouisa", // default pet until function available to render first pet of user
+
     initialize: function() {
+      self = this;
       this.render();
     },
 
     render: function() {
       if (Parse.User.current()) {
         console.log(Parse.User.current().getUsername());
-        new LinkView();
+        app_router.navigate('//'+self.pet);
       } else {
         console.log('No user signed in. Proceeding to splash screen.');
         new SplashView();
