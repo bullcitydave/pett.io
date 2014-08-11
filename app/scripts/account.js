915be3f19f211f2b6514d9886e91a302bef33d11
Dave Seidman <bullcitydave@gmail.com>
2014-08-11 17:41:00 -0400

    Account view successfully queries Pet class to return list of pets based on criteria of pointer to user

--- app/scripts/account.js
+++ app/scripts/account.js
@@ -6,7 +6,8 @@ var AccountView = Parse.View.extend({
   el: "#main-container",
 
   initialize: function() {
-    console.log("Account view initialized")
+    console.log("Account view initialized");
+    x=this;
     _.bindAll(this, "createPet");
     this.render();
   },
@@ -19,5 +20,34 @@ var AccountView = Parse.View.extend({
 
   render: function() {
     this.$el.html(_.template($("#account-template").html(), ({"userName": Parse.User.current().getUsername()})));
+
+    var ppQuery = new Parse.Query(Pet);
+    // ppQuery.equalTo("person", Parse.User.current().getUsername());
+
+    ppQuery.equalTo("person", {
+        __type: "Pointer",
+        className: "_User",
+        objectId: Parse.User.current().getUsername()
+    });
+
+    console.log('ppQuery: ',ppQuery);
+
+    ppQuery.find({
+      success: function(results) {
+          x.listPets(results);
+      },
+
+      error: function(error) {
+          alert('Error!');
+        }
+      });
+    },
+
+  listPets: function(results) {
+     for (var i = 0; i < results.length ; i++) {
+        console.log(results[i].attributes.name);
+
+       $('.user-profile').append(_.template('<p>' + results[i].attributes.name + '</p>'));
+     }
   }
 });
