3733ac518abbc6dfc79b69a8fb94edeca1972413
Dave Seidman <bullcitydave@gmail.com>
2014-09-03 01:33:48 -0400

    After updating Parse data for new ParsePic petUniqueName property (must encode this to set automatically), and updating browse queries, browse renders all pets with same name as different pets'

--- app/scripts/browse.js
+++ app/scripts/browse.js
@@ -52,6 +52,7 @@ var BrowseView = Parse.View.extend({
     petsQuery.find({
       success: function(results) {
 
+
     $('#browse-container').imagesLoaded(function() {
       $('#browse-container').masonry({
             columnwidth: 200,
@@ -65,11 +66,12 @@ var BrowseView = Parse.View.extend({
     var petUName = results[i].attributes.uniqueName;
 
     var ppQuery1 = new Parse.Query(ParsePic);
-    ppQuery1.equalTo("petname",petUName);
-    ppQuery1.equalTo("size","medium");
+    ppQuery1.equalTo("petUniqueName",petUName);
+    ppQuery1.containedIn("size",
+                ["medium", "undefined"]);
 
     var ppQuery2 = new Parse.Query(ParsePic);
-    ppQuery2.equalTo("petname",petUName);
+    ppQuery2.equalTo("petUniqueName",petUName);
     ppQuery2.doesNotExist("size");
 
     var ppQuery =  new Parse.Query.or(ppQuery1, ppQuery2);
