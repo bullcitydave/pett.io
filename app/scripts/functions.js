f58884fb9978984f55c34d900edbee94e650c963
Dave Seidman <bullcitydave@gmail.com>
2014-09-15 14:34:21 -0400

    Initial check to see if internet connetion is even available

--- app/scripts/functions.js
+++ app/scripts/functions.js
@@ -1,7 +1,13 @@
 // Initialize parse and support functions
 
 
+
+try {
   Parse.initialize("9MAJwG541wijXBaba0UaiuGPrIwMQvLFm4aJhXBC", "DGHfzC6pvsu3P94CsFDReHIpwB3CUf7Pe0dP4WiP");
+}  catch (exception)
+  {
+    alert('It looks like there may be a problem with either your internet connection or pett.io\'s ability to reach its database server. Please try again shortly.' )
+  };
 
 
 
