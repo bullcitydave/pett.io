f4dd80a5d54c023abd727a1c25c1323864d5ede8
Dave Seidman <bullcitydave@gmail.com>
2014-10-11 16:58:02 -0400

    Map WIP, pulling 1 rec, working on marker

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -3,14 +3,21 @@ var MapView = Parse.View.extend ({
   el: "body",
 
   initialize: function() {
-    $(this.el).append($('#map-template'));
+
+
+
+    $('body').append(_.template(($('#map-template').html())));
 
     console.log('Getting map...');
 
     map = this;
 
+    defLat = 37.09024;
+    defLng = -95.712891;
+
     $('#main-container').css("opacity", .3);
 
+    google.maps.event.addDomListener(window, 'load', this.mapInitialize);
 
     var query = new Parse.Query(Pet);
     var tempGeo = new Parse.GeoPoint(45,-45);
@@ -18,8 +25,7 @@ var MapView = Parse.View.extend ({
     query.find({
       success: function(results) {
         console.log("Successfully retrieved " + results.length+ ". Attempting to render...");
-        map.render(results[0].attributes);
-        console.log(results[0].attributes);
+        map.render(results);
       },
       error: function(error) {
         console.log("Error: " + error.code + " " + error.message);
@@ -38,6 +44,47 @@ var MapView = Parse.View.extend ({
     },
 
     render: function(data){
-      console.log('render');
+      var geocoder;
+      var map;
+      var marker;
+      this.mapInitialize();
+      this.markLocation(data[0].attributes.geoLocation);
+    },
+
+    mapInitialize: function () {
+      geocoder = new google.maps.Geocoder();
+      var latlng = new google.maps.LatLng(defLat,defLng);
+      var mapOptions = {
+        zoom: 4,
+        center: latlng,
+        mapTypeId: 'roadmap'
+      }
+      petMap = new google.maps.Map(document.getElementById('map-container'), mapOptions);
+    },
+
+
+    markLocation: function(geoPoint) {
+      var lat = geoPoint.latitude;
+      var lng = geoPoint.longitude;
+      var infowindow = new google.maps.InfoWindow();
+      var latlng = new google.maps.LatLng(lat, lng);
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (results[1]) {
+            // petMap.setZoom(11);
+            marker = new google.maps.Marker({
+                map: petMap,
+                position: results[1].geometry.location,
+                title: results[1].formatted_address
+            });
+            // infowindow.setContent(results[1].formatted_address);
+            // infowindow.open(petMap, marker);
+          } else {
+            alert('No results found');
+          }
+        } else {
+          alert('Geocoder failed due to: ' + status);
+        }
+      });
     }
 });
