5c11b9e17261f89aa1a8899c27a0f7795eaa9922
Dave Seidman <bullcitydave@gmail.com>
2014-10-12 21:15:40 -0400

    Maps WIP

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,28 +1,51 @@
 var MapView = Parse.View.extend ({
 
-  el: "body",
+  // el: "body",
 
   initialize: function() {
 
 
-    $('#main-header').addClass('standard');
-    $('#main-container').removeClass('splash');
-    $('#main-container').addClass('standard');
-    $('#main-container').removeClass('browse');
-    $('#main-container').html('');
+    // $('#main-header').addClass('standard');
+    // $('#main-container').removeClass('splash');
+    // $('#main-container').addClass('standard');
+    // $('#main-container').removeClass('browse');
+    // $('#main-container').html('');
+    $('#main-container').remove();
+    $('#main-header').remove();
 
 
     console.log('Getting map...');
 
-    map = this;
+    // map = this;
 
-    defLat = 37.09024;
-    defLng = -95.712891;
+    var geocoder;
+    var myMap;
+    var marker;
 
-    $('#main-container').append(_.template($('#map-template').html()));
-    $('.map-outer').show();
+    // defLat = 37.09024;
+    // defLng = -95.712891;
 
-    google.maps.event.addDomListener(window, 'load', this.mapInitialize);
+    // $('#main-container').append(_.template($('#map-template').html()));
+    // $('#map-canvas').show();
+
+
+var latlng = new google.maps.LatLng(35,-96);
+var mapOptions = {
+  zoom: 4,
+  center: latlng,
+  mapTypeId: 'roadmap'
+}
+myMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+marker = new google.maps.Marker({
+      map: myMap,
+      position: latlng,
+      title:"Hello World!",
+      visible: true
+});
+
+
+
+    // google.maps.event.addDomListener(window, 'load', this.mapInitialize);
 
     var query = new Parse.Query(Pet);
     var tempGeo = new Parse.GeoPoint(45,-45);
@@ -30,7 +53,7 @@ var MapView = Parse.View.extend ({
     query.find({
       success: function(results) {
         console.log("Successfully retrieved " + results.length+ ". Attempting to render...");
-        map.render(results);
+        // map.render(results);
       },
       error: function(error) {
         console.log("Error: " + error.code + " " + error.message);
@@ -38,39 +61,28 @@ var MapView = Parse.View.extend ({
     });
   },
 
-    events: {
-
-      'click .close'    : 'closeMap'
-
-    },
-
-    closeMap: function(e) {
-      map.el.fadeOut(750);
-    },
-
     render: function(data){
-      var geocoder;
-      var myMap;
-      var marker;
+
       this.mapInitialize();
-      this.markLocation(data[0].attributes.geoLocation);
+      // this.markLocation(data[0].attributes.geoLocation);
     },
 
     mapInitialize: function () {
-      geocoder = new google.maps.Geocoder();
-      var latlng = new google.maps.LatLng(defLat,defLng);
-      var mapOptions = {
-        zoom: 4,
-        center: latlng,
-        mapTypeId: 'roadmap'
-      }
-      myMap = new google.maps.Map(document.getElementById('map-container'), mapOptions);
-      marker = new google.maps.Marker({
-            map: myMap,
-            position: latlng,
-            title:"Hello World!",
-            visible: true
-      });
+      // geocoder = new google.maps.Geocoder();
+      // var latlng = new google.maps.LatLng(defLat,defLng);
+      // var latlng = new google.maps.LatLng(35,-96);
+      // var mapOptions = {
+      //   zoom: 4,
+      //   center: latlng,
+      //   mapTypeId: 'roadmap'
+      // }
+      // myMap = new google.maps.Map(document.getElementById('map-container'), mapOptions);
+      // marker = new google.maps.Marker({
+      //       map: myMap,
+      //       position: latlng,
+      //       title:"Hello World!",
+      //       visible: true
+      // });
     },
 
 
@@ -89,7 +101,8 @@ var MapView = Parse.View.extend ({
             marker2 = new google.maps.Marker({
                 map: myMap,
                 position: results[1].geometry.location,
-                title: results[1].formatted_address
+                title: results[1].formatted_address,
+                zindex: 9999999
             });
             infowindow.setContent(results[1].formatted_address);
             infowindow.open(myMap, marker2);
