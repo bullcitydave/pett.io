3706013fe8b516132672db465aefd9584edd00ee
Dave Seidman <bullcitydave@gmail.com>
2014-11-16 15:22:44 -0500

    Refactoring browse.js WIP

--- app/scripts/browse.js
+++ app/scripts/browse.js
@@ -41,98 +41,124 @@ var BrowseView = Parse.View.extend({
     $('#log-out').show();
     $('body').addClass('darkbg');
     APP.main.append("<div id='browse-container'></div>");
-    browseSelf.render();
+    browseSelf.render().squeeze();
   },
-
-
-
 
   render: function() {
 
-  if (Parse.User.current() !== null)
-    {
-      $('.site-visitor').hide();
-      $('.site-user').show();
-    }
-  else {
-      $('.site-user').hide();
-      $('.site-visitor').show();
-  }
-    var petsQuery = new Parse.Query(Pet);
-    petsQuery.limit(1000);
-    petsQuery.select("uniqueName");
-    petsQuery.descending("updatedAt");
-    petsQuery.find({
-      success: function(results) {
-
-
-
-  for (var i = 0; i < results.length ; i++){
-    var petUName = results[i].attributes.uniqueName;
-    var ppQuery1 = new Parse.Query(ParsePic);
-    ppQuery1.equalTo("petUniqueName",petUName);
-    ppQuery1.containedIn("size",
-                ["original", "undefined"]);
-
-    var ppQuery2 = new Parse.Query(ParsePic);
-    ppQuery2.equalTo("petUniqueName",petUName);
-    ppQuery2.doesNotExist("size");
-
-    var ppQuery =  new Parse.Query.or(ppQuery1, ppQuery2);
-
-
-
-    ppQuery.ascending("petname");
-
-
-    ppQuery.find({
-      success: function(results) {
-          if (results.length > 0) {
-            var randomImg = Math.floor(Math.random() * (results.length));
-            browseSelf.showPics(results[randomImg]);
-        };
-      }
-    });
-  }
-
-
-},
-  error: function(error) {
-          console.log("Error: " + error.code + " " + error.message);
+      if (Parse.User.current() !== null)
+        {
+          $('.site-visitor').hide();
+          $('.site-user').show();
         }
+      else {
+          $('.site-user').hide();
+          $('.site-visitor').show();
+      }
+        var petsQuery = new Parse.Query(Pet);
+        petsQuery.limit(1000);
+        petsQuery.select("uniqueName");
+        petsQuery.descending("updatedAt");
+        petsQuery.find({
+          success: function(results) {
+              browseSelf.getPics(results);
+              return this;
+            },
+          error: function(error) {
+              console.log("Error: " + error.code + " " + error.message);
+          }
       });
+      return this;
+  },
+
+  getPics: function(results) {
+      for (var i = 0; i < results.length ; i++){
+          var petUName = results[i].attributes.uniqueName;
+          var ppQuery1 = new Parse.Query(ParsePic);
+          ppQuery1.equalTo("petUniqueName",petUName);
+          ppQuery1.containedIn("size",
+                      ["original", "undefined"]);
+
+          var ppQuery2 = new Parse.Query(ParsePic);
+          ppQuery2.equalTo("petUniqueName",petUName);
+          ppQuery2.doesNotExist("size");
+
+          var ppQuery =  new Parse.Query.or(ppQuery1, ppQuery2);
+
+          ppQuery.ascending("petname");
+
+          ppQuery.find({
+            success: function(results) {
+                if (results.length > 0) {
+                  var randomImg = Math.floor(Math.random() * (results.length));
+                  browseSelf.showPics(results[randomImg], i);
+              };
+            }
+          });
+      }
+  },
 
 
-},
+  showPics: function(results) {
+      var browseView = $('#browse-template').html();
+      $('#browse-container').append(_.template(browseView,results.attributes));
+  },
 
 
-showPics: function(results) {
-    var browseView = $('#browse-template').html();
-    $('#browse-container').append(_.template(browseView,results.attributes));
-},
+  hoverBox: function(event) {
+      // $('.hovering').removeClass('hovering');
+      var targetBox = $(event.target).parent().parent();
+      targetBox.addClass('hovering');
+      // need separate for 1st el, top x els, last of row 1
+      // bottom left, bottom, bottom-right
+      // left, right
+      // everything else
+      if ((targetBox.position().top < 100)) {
+        targetBox.switchClass('hovering','hovering-down');
+      }
+      if ((targetBox.parent().height() - targetBox.position().top) < 300) {
+        targetBox.switchClass('hovering','hovering-up');
+      }
 
+  },
 
-hoverBox: function(event) {
-    // $('.hovering').removeClass('hovering');
-    var targetBox = $(event.target).parent().parent();
-    targetBox.addClass('hovering');
-    // need separate for 1st el, top x els, last of row 1
-    // bottom left, bottom, bottom-right
-    // left, right
-    // everything else
-    if ((targetBox.position().top < 100)) {
-      targetBox.switchClass('hovering','hovering-down');
+  leaveBox: function(event) {
+      var targetBox = $(event.target).parent().parent();
+      // $('*[class*="hovering"]').removeClass('hovering hovering-up hovering-down');
+      targetBox.removeClass('hovering hovering-up hovering-down');
+  },
+
+  squeeze: function() {
+    console.log('Squeezing...');
+    var rowWidth = 0;
+    var A = [];
+    var width = window.innerWidth;
+    var insertAfter;
+    var insertThis;
+    for (i = 0; i < $('.pet-box').length; i++ ) {
+        A[i] = ($('.pet-box').eq(i).width())+38;
+
+        if ((rowWidth + A[i]) > width) {
+          insertAfter = (i-1);
+          var extraSpace = width - rowWidth;
+          // console.log('extraspace ', extraSpace);
+          for (j = i; j < $('.pet-box').length; j++) {
+            if (($('.pet-box').eq(j).width()+38) < extraSpace) {
+              console.log(j + ' ' + $('.pet-box').eq(j).width());
+              insertThis = j;
+              $('.pet-box').eq(insertThis).insertAfter($('.pet-box').eq(insertAfter));
+              browseSelf.squeeze();
+              return false;
+             }
+           }
+           rowWidth = A[i];
+          //  console.log('rowWidth: ', rowWidth);
+        }
+        else {
+          rowWidth += A[i];
+          // console.log('rowWidth: ', rowWidth);
+        }
     }
-    if ((targetBox.parent().height() - targetBox.position().top) < 300) {
-      targetBox.switchClass('hovering','hovering-up');
-    }
-
-},
-
-leaveBox: function(event) {
-    var targetBox = $(event.target).parent().parent();
-    // $('*[class*="hovering"]').removeClass('hovering hovering-up hovering-down');
-    targetBox.removeClass('hovering hovering-up hovering-down');
-}
+  }
 
 });
---
+++ app/scripts/squeeze.js
@@ -0,0 +1,35 @@
+function squeeze() {
+  var rowWidth = 0;
+  var A = [];
+  var width = window.innerWidth;
+  var insertAfter;
+  var insertThis;
+  for (i = 0; i < $('.pet-box').length; i++ ) {
+      A[i] = ($('.pet-box').eq(i).width())+38;
+
+      if ((rowWidth + A[i]) > width) {
+        insertAfter = (i-1);
+        var extraSpace = width - rowWidth;
+        // console.log('extraspace ', extraSpace);
+        for (j = i; j < $('.pet-box').length; j++) {
+          if (($('.pet-box').eq(j).width()+38) < extraSpace) {
+            console.log(j + ' ' + $('.pet-box').eq(j).width());
+            insertThis = j;
+            $('.pet-box').eq(insertThis).insertAfter($('.pet-box').eq(insertAfter));
+            squeeze();
+            return false;
+           }
+         }
+         rowWidth = A[i];
+        //  console.log('rowWidth: ', rowWidth);
+      }
+      else {
+        rowWidth += A[i];
+        // console.log('rowWidth: ', rowWidth);
+      }
+  }
+}
+
+// next step
+// $('.pet-box').eq(3).css("margin-right","+=5")
+// take the boxes in the row and increment margin based on extra space available to allocate
