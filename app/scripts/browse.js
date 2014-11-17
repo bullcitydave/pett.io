0342b77b0a11e834c4c936cc87e6934505df81ac
Dave Seidman <bullcitydave@gmail.com>
2014-11-17 01:28:04 -0500

    Browse.js - squeeze implemented; sitewide font change

--- app/scripts/browse.js
+++ app/scripts/browse.js
@@ -6,8 +6,7 @@ var BrowseView = Parse.View.extend({
 
   events: {
     "mouseover .pet-pic"    : "hoverBox",
-    "mouseout  .pet-pic"    : "leaveBox"
-
+    "mouseout  .pet-pic"    : "leaveBox",
   },
 
   initialize: function(tag) {
@@ -51,8 +50,9 @@ var BrowseView = Parse.View.extend({
     parsePics = parsePicQuery.collection();
 
 
-
+    d = new $.Deferred();
     browseSelf.render();
+
   },
 
   render: function() {
@@ -87,10 +87,14 @@ var BrowseView = Parse.View.extend({
 
       var names = collection.pluck('petUniqueName');
       var uniqueNames = _.countBy(names);
+      browseSelf.imageCount = Object.keys(uniqueNames).length;
+      $(document).on("load", ".pet-box[Object.keys(uniqueNames.length)]", function(){
+        browseSelf.squeeze();
+        })
       var petPics = collection.models;
 
       _.each(Object.keys(uniqueNames), function(name){
-        console.log(name + "--" + uniqueNames[name]);
+        // console.log(name + "--" + uniqueNames[name]);
         // var petPhotos = _.where(collection)
         var petPics = collection.filter(function(petPic) {
           return petPic.get('petUniqueName') === name; });
@@ -98,6 +102,8 @@ var BrowseView = Parse.View.extend({
         var randomPic = Math.floor(Math.random() * numPics);
         browseSelf.showPics(petPics[randomPic]);
       })
+
+      // browseSelf.squeeze();
 
       //
       // var randomImg = Math.floor(Math.random() * (results.length));
@@ -143,6 +149,19 @@ var BrowseView = Parse.View.extend({
   showPics: function(results) {
       var browseTemplate = $('#browse-template').html();
       $('#browse-container').append(_.template(browseTemplate,results.attributes));
+      var imgLoad = imagesLoaded('#browse-container');
+      imgLoad.on( 'always', function() {
+        // console.log( imgLoad.images.length + ' images loaded' );
+        if (imgLoad.images.length === browseSelf.imageCount) {
+          browseSelf.squeeze();
+        };
+    // detect which image is broken
+    // for ( var i = 0, len = imgLoad.images.length; i < len; i++ ) {
+    //   var image = imgLoad.images[i];
+    //   var result = image.isLoaded ? 'loaded' : 'broken';
+    //   // console.log( 'image is ' + result + ' for ' + image.img.src );
+    // }
+  });
       // if (petCount === 10) { d1.resolve(); };
   },
 
@@ -183,7 +202,7 @@ var BrowseView = Parse.View.extend({
   },
 
   squeeze: function() {
-    console.log('Squeezing...');
+    // console.log('Squeezing...');
     var rowWidth = 0;
     var A = [];
     var width = window.innerWidth;
@@ -198,7 +217,7 @@ var BrowseView = Parse.View.extend({
           // console.log('extraspace ', extraSpace);
           for (j = i; j < $('.pet-box').length; j++) {
             if (($('.pet-box').eq(j).width()+38) < extraSpace) {
-              console.log(j + ' ' + $('.pet-box').eq(j).width());
+              // console.log(j + ' ' + $('.pet-box').eq(j).width());
               insertThis = j;
               $('.pet-box').eq(insertThis).insertAfter($('.pet-box').eq(insertAfter));
               browseSelf.squeeze();
@@ -214,5 +233,7 @@ var BrowseView = Parse.View.extend({
         }
     }
   }
+
+
 
 });
